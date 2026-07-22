edd32b9bead4fd51092f7941b89bd591ab44c1d9
arroio <juan.arroyo@iteso.mx>
2026-07-22 16:20:16 -0600

    fixed missing id on fact table

--- models/silver/sale_order_line_model.sql
+++ models/silver/sale_order_line_model.sql
@@ -13,12 +13,13 @@ MODEL(
 );
 
 
-SELECT sol.id AS id, sol.product_id, sol.state, so.payment_term_id, sol.invoice_status, sol.product_uom_qty,
+SELECT sol.id AS id, sol.product_id, rp.id AS customer_id  ,so.payment_term_id, sol.state, ,sol.invoice_status, sol.product_uom_qty,
        (sol.qty_invoiced + sol.qty_delivered + sol.qty_to_invoice) AS qty, so.name,
-        sol.price_unit, sol.price_total, sol.price_subtotal, sol.create_date -- , to_char(sol.create_date,'YYMMDD') AS date_id,
+        sol.price_unit, sol.price_total, sol.price_subtotal, sol.create_date, to_char(sol.create_date,'YYMMDD') AS date_id,
        
 FROM bronze.raw__sale_order_line sol
 JOIN bronze.raw__sale_order so ON sol.order_id = so.id
-JOIN bronze.raw__product_product pp on sol.product_id = pp.id
-JOIN bronze.raw__product_template pt on pp.product_tmpl_id = pt.id
+JOIN bronze.raw__product_product pp ON sol.product_id = pp.id
+JOIN bronze.raw__product_template pt ON pp.product_tmpl_id = pt.id
+JOIN bronze.raw__rest_partner rp ON sol.order_partner_id = rp.id
 WHERE sol.company_id = 5
